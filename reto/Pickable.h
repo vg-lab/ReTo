bda0e799faf036b090d40184a6b8cd37f9b8a266
ccrisrober <ccrisrober@gmail.com>
2016-06-23 13:35:29 +0200

    Refactorize Simple Picking with Juanjo and Sergio ideas.

--- reto/Pickable.h
+++ reto/Pickable.h
@@ -19,59 +19,21 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  */
+
 #ifndef __RETO__PICKABLE__
 #define __RETO__PICKABLE__
 
-#include <vector>
-#include "ProgramShader.h"
-
-#include <vector>
-#include <set>
-#include "Model.h"
-#include "ProgramShader.h"
-
-#include <reto/api.h>
-
-namespace reto
+class Pickable
 {
-  typedef struct {
-    int x;
-    int y;
-  } Point;
+public:
+	Pickable( void );
+	virtual ~Pickable ( void ); 
+	virtual unsigned int sendId ( unsigned int currentOffset );
+	virtual void render ( void ) = 0;
 
-  class Pickable
-  {
-  public:
-    RETO_API
-    Pickable();
-
-    /**
-     * Method to find front object in a specific point
-     * @param point: Point
-     * @param models: List of elements to render
-     * @param vp: ModelView matrix
-     * @return int: Indice that is visible
-     */
-    RETO_API
-    int click(Point point, 
-      const std::vector<reto::Model*>& models, std::vector<float>& vp);
-
-
-    /**
-     * Method to find front object in a specific area
-     * @param minPoint: minPoint
-     * @param maxPoint: maxPoint
-     * @param models: List of elements to render
-     * @param vp: ModelView matrix
-     * @return std::set<unsigned int> Indices that objects are visibles
-     */
-    RETO_API
-    std::set<unsigned int> area(Point minPoint, Point maxPoint, 
-      const std::vector<reto::Model*>& models, std::vector<float>& vp);
-
-    protected:
-      reto::ProgramShader _program;
-  };
-}
+protected:
+	int _numIds = 1;
+	void setNumIDs ( int numIds );
+};
 
 #endif // __RETO__PICKABLE__
